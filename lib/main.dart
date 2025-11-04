275121fba35578434515e576dbb59edc785705d5
khalisharose <khalisha.roselani@cs.ui.ac.id>
2025-11-04 22:15:59 +0700

    Tugas 7

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:supercopa/menu.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: ' Supercopa ',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.blue)
+            .copyWith(secondary: Colors.blueAccent[400]),
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
